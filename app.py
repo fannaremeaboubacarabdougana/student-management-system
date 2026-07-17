a939c469e1815f9b408ba4eec053c320168cb234
Fannareme Aboubacar <fannareme.abdou@ashesi.edu.gh>
2026-07-17 02:43:07 +0100

    Display students in a table

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from config import Config
-from models import db
+from models import db, Student
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -11,7 +11,38 @@ db.init_app(app)
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
+
+
+@app.route("/students/add", methods=["GET", "POST"])
+def add_student():
+
+    if request.method == "POST":
+        student = Student(
+            student_id=request.form["student_id"],
+            first_name=request.form["first_name"],
+            last_name=request.form["last_name"],
+            email=request.form["email"],
+            phone=request.form["phone"],
+            program=request.form["program"],
+            level=request.form["level"],
+        )
+
+        db.session.add(student)
+        db.session.commit()
+
+        return redirect(url_for("home"))
+
+    return render_template("add_student.html")
+
+
+@app.route("/students")
+def students():
+
+    students = Student.query.all()
+
+    return render_template("students.html", students=students)
 
 
 with app.app_context():
